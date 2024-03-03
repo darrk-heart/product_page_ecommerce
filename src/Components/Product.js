30f89c387e2337619c579b47c9424e565516e6d8
darrk-heart <prospaul999@gmail.com>
2024-03-03 18:13:25 +0000

    working on final design

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -8,6 +8,9 @@ import three from "./images/image-product-3.jpg";
 import threethumb from "./images/image-product-3-thumbnail.jpg";
 import four from "./images/image-product-4.jpg";
 import fourthumb from "./images/image-product-4-thumbnail.jpg";
+import cart from "./images/icon-cart.svg";
+import minus from "./images/icon-minus.svg";
+import plus from "./images/icon-plus.svg";
 
 function Product() {
   return (
@@ -27,8 +30,32 @@ function Product() {
           <img src={fourthumb} alt="imagefourthumbnail" />
         </div>
       </div>
-      <div>
-        
+      <div className="product-text">
+        <div>
+          <h5>SNEAKER COMPANY</h5>
+          <h1>Fall Limited Edition Sneakers</h1>
+          <p>
+            These low-profile sneakers are your perfect casual wear companion.
+            Featuring a durable rubber outer sole, they'll withstand everything
+            the weather can offer.
+          </p>
+        </div>
+        <div>
+          <h2>$125.00</h2>
+          <span>50%</span>
+          <span>$250.00</span>
+          <div>
+            <div>
+              <img src={minus} alt="minus sign" />
+              <span>0</span>
+              <img src={plus} alt="plus sign" />
+            </div>
+            <div>
+              <img src={cart} alt="cart" />
+              <span>Add to cart</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

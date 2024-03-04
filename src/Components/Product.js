1837995f50c0822065ee7bf3933901bd486f7ae5
darrk-heart <prospaul999@gmail.com>
2024-03-04 07:53:24 +0000

    still working on ui

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -35,14 +35,16 @@ function Product() {
           <h5>SNEAKER COMPANY</h5>
           <h1>Fall Limited Edition Sneakers</h1>
           <p>
-            These low-profile sneakers are your perfect casual wear companion.
-            Featuring a durable rubber outer sole, they'll withstand everything
-            the weather can offer.
+            These low-profile sneakers are your perfect casual wear <br />{" "}
+            companion. Featuring a durable rubber outer sole, they'll <br />{" "}
+            withstand everything the weather can offer.
           </p>
         </div>
         <div className="price-text">
-          <h2>$125.00</h2>
-          <span>50%</span>
+          <div className="dollars">
+            <h2>$125.00</h2>
+            <span>50%</span>
+          </div>
           <span>$250.00</span>
           <div>
             <div>

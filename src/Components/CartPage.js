9c7d7c0218d1553d5c39e33e4a2f3a294ee58748
darrk-heart <prospaul999@gmail.com>
2024-03-06 09:04:33 +0000

    almost done with cartpage

--- src/Components/CartPage.js
+++ src/Components/CartPage.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./CartPage.css";
+import bin from "./images/icon-delete.svg";
+import one from "./images/image-product-1.jpg";
 
 function CartPage() {
   return (
     <div className="cartpage">
       <div className="cart-head">Cart</div>
       <div className="cart-body">
-        <span>Your cartpage is empty</span>
+        {/* <span>Your cartpage is empty</span> */}
+
+        <div className="amount">
+          <div className="amount-check">
+            <img src={one} alt="shoe capture" />
+            <div className="amount-check-text">
+              <span>Fall Limited Edition Sneakers</span>
+              <div>
+                <span>$125.00 x 3</span>
+                <span className="total">$375.00</span>
+              </div>
+            </div>
+          </div>
+          <img src={bin} alt="delete" />
+        </div>
+        <div className="checkout">Checkout</div>
       </div>
     </div>
   );
